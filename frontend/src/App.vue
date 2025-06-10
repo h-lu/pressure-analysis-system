38a0c02ec23669b6c5018c50a0632c3154b06ce6
Lu, Haibo <luhaibo1985@gmail.com>
2025-06-10 12:51:33 +0800

    添加前端App组件

---
+++ frontend/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+        <h1>压力采集数据分析系统</h1>
+      </el-header>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+.el-header {
+  background-color: #409EFF;
+  color: white;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-header h1 {
+  margin: 0;
+}
+</style>
